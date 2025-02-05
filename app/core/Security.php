e02ba567e7e3e525ab6d5d737d1447338643c123
Ali Yara <ali.yara.fr@gmail.com>
2025-02-05 16:53:34 +0100

    Add Security class and methods from password

---
+++ app/core/Security.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Core;
+
+class Security
+{
+    public static function hashPassword($password)
+    {
+        return password_hash($password, PASSWORD_BCRYPT);
+    }
+
+    public static function verifyPassword($password, $hash)
+    {
+        return password_verify($password, $hash);
+    }
+
+}
