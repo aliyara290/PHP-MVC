00a5dbb7d28184046bc8aac184f383575bab4510
Ali Yara <ali.yara.fr@gmail.com>
2025-02-05 16:54:26 +0100

    Add session class with multiple method for reusablity

---
+++ app/core/Session.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Core;
+
+class Session
+{
+    public static function start()
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public static function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key)
+    {
+        return $_SESSION[$key] ?? null;
+    }
+
+    public static function has($key)
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public static function remove($key)
+    {
+        if (isset($_SESSION[$key])) {
+            unset($_SESSION[$key]);
+        }
+    }
+
+    public static function destroy()
+    {
+        if (session_status() != PHP_SESSION_NONE) {
+            session_unset();
+            session_destroy();
+        }
+    }
+}
