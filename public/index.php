80e2a7023e40ac2260b044525e55b44f15b3c462
Ali Yara <ali.yara.fr@gmail.com>
2025-02-05 16:58:54 +0100

    add more routes

--- public/index.php
+++ public/index.php
@@ -2,11 +2,21 @@
 require __DIR__ . "/../config/Database.php";
 require __DIR__ . "/../vendor/autoload.php";
 use App\Core\Router;
+use App\Core\Session;
 use App\Controllers\HomeController;
 use App\Controllers\ArticleController;
+use App\Controllers\SignUpController;
+use App\Controllers\SignInController;
+use App\Controllers\AuthController;
+
+Session::start();
 $router = new Router();
-$router->get("/", ArticleController::class, "showAllArticles");
-$router->get("/sign-in", HomeController::class, "signIn");
-$router->get("/sign-up", HomeController::class, "signUp");
-$router->get("/article", HomeController::class, "create");
+$router->get("/", HomeController::class, "showAllArticles");
+$router->get("/article", ArticleController::class, "readByCondition");
+$router->get("/sign-in", SignInController::class, "signInPage");
+$router->post("/sign-in", SignInController::class, "logIn");
+$router->get("/sign-up", SignUpController::class, "signUp");
+$router->post("/sign-up", SignUpController::class, "register");
+$router->get("/logout", AuthController::class, "logout");
+$router->get("/admin/dashboard", HomeController::class, "showAllArticles");
 $router->dispatch();
