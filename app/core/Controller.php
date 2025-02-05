dd0fe3dcb304212ba9301ad269976a0453e51f46
Ali Yara <ali.yara.fr@gmail.com>
2025-02-05 16:50:44 +0100

    add model and redirect methods in controller class

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,8 +3,22 @@ namespace App\Core;
 use App\Core\View;
 
 class Controller {
-    public function render($view, $data = []) {
-        extract($data);
-        require "../views/front/$view.php";
+    public function model($model)
+    {
+        $modelPath = "../app/models/" . ucfirst($model) . ".php";
+
+        if (file_exists($modelPath)) {
+            require_once $modelPath;
+            $modelClass = "App\\Models\\" . ucfirst($model);
+            return new $modelClass();
+        }
+
+        die("Model '$model' not found.");
+    }
+    
+    public function redirect($url)
+    {
+        header("Location: " . $url);
+        exit();
     }
 }
